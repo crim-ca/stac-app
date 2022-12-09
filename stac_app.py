b43c649c66af01ecc007f658422bb61c3c49026e
Mathieu Provencher <>
2022-12-09 17:45:03 -0500

    clean

--- stac_app.py
+++ stac_app.py
@@ -59,14 +59,6 @@ api = StacApi(
 )
 app = api.app
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
 
 @app.on_event("startup")
 async def startup_event():
